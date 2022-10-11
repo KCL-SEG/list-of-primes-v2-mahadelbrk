86fae7e0f156dd7485faee2706fa0483228b08b0
root <root@DESKTOP-99L9KP9.localdomain>
2022-10-11 12:54:43 +0100

    prime numbers

--- primes.py
+++ primes.py
@@ -2,16 +2,30 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 
-def primes(number_of_primes):
-    list = []
-    for possiblePrime in range(2, 90):
+# def primes(number_of_primes):
+#     list = []
+#     for possiblePrime in range(2, 90):
     
-        isPrime = True
-        for num in range(2, possiblePrime):
-            if possiblePrime % num == 0:
-                isPrime = False
+#         isPrime = True
+#         for num in range(2, possiblePrime):
+#             if possiblePrime % num == 0:
+#                 isPrime = False
             
-        if isPrime:
-            list.append(possiblePrime)
+#         if isPrime:
+#             list.append(possiblePrime)
             
-    return list[0:number_of_primes]
+#     return list[0:number_of_primes]
+def primes (number_of_primes) :
+    list = []
+    num = 2
+    i=0
+    while i < number_of_primes:
+        prime = True
+        for j in range (2, num) :
+            if num % j == 0:
+                prime = False
+        if prime:
+            list.append (num)
+            i=i1
+        num = num +1
+    return list
